3b6ded605dfc045e95d97407023bb81adb83d65e
Dan Grøndahl Glavind <dan.glavind@eficode.com>
2022-12-07 11:03:06 +0100

    add ginkgo tests

--- controllers/gateway_controller_test.go
+++ controllers/gateway_controller_test.go
@@ -5,15 +5,31 @@ import (
 	//"reflect"
 	//"time"
 
+	"context"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	//v1 "k8s.io/api/core/v1"
-	//metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/yaml"
+
 	//"k8s.io/apimachinery/pkg/types"
 	gateway "sigs.k8s.io/gateway-api/apis/v1beta1"
 )
 
-var _ = Describe("CronJob controller", func() {
+const gatewayManifest string = `
+apiVersion: gateway.networking.k8s.io/v1beta1
+kind: Gateway
+metadata:
+  name: foo-gateway
+  namespace: default
+spec:
+  gatewayClassName: default
+  listeners:
+  - name: prod-web
+    port: 80
+    protocol: HTTP
+    hostname: example.com
+`
+
+var _ = Describe("Gateway controller", func() {
 
 	Context("When building Gateway resource from input Gateway", func() {
 		It("Should return a new Gateway", func() {
@@ -22,6 +38,16 @@ var _ = Describe("CronJob controller", func() {
 			gateway := &gateway.Gateway{}
 			gw_out := BuildGatewayResource(gateway)
 			Expect(gw_out).NotTo(BeNil())
+		})
+	})
+
+	Context("When applying a parent Gateway", func() {
+		It("Should create a child gateway", func() {
+			ctx := context.Background()
+			gateway := &gateway.Gateway{}
+			_ = yaml.Unmarshal([]byte(gatewayManifest), gateway)
+			Expect(k8sClient.Create(ctx, gateway)).Should(Succeed())
+			Expect(string(gateway.Spec.GatewayClassName)).To(Equal("default"))
 		})
 	})
 })
--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -17,7 +17,10 @@ limitations under the License.
 package controllers
 
 import (
+	"context"
 	"path/filepath"
+	ctrl "sigs.k8s.io/controller-runtime"
+	gateway "sigs.k8s.io/gateway-api/apis/v1beta1"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -35,9 +38,13 @@ import (
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-var cfg *rest.Config
-var k8sClient client.Client
-var testEnv *envtest.Environment
+var (
+	cfg       *rest.Config
+	k8sClient client.Client
+	testEnv   *envtest.Environment
+	ctx       context.Context
+	cancel    context.CancelFunc
+)
 
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -47,6 +54,8 @@ func TestAPIs(t *testing.T) {
 
 var _ = BeforeSuite(func() {
 	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+
+	ctx, cancel = context.WithCancel(context.TODO())
 
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
@@ -60,15 +69,35 @@ var _ = BeforeSuite(func() {
 	Expect(err).NotTo(HaveOccurred())
 	Expect(cfg).NotTo(BeNil())
 
+	err = gateway.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
 	//+kubebuilder:scaffold:scheme
 
 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
 	Expect(err).NotTo(HaveOccurred())
 	Expect(k8sClient).NotTo(BeNil())
 
+	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
+		Scheme: scheme.Scheme,
+	})
+	Expect(err).ToNot(HaveOccurred())
+
+	err = (&GatewayReconciler{
+		Client: k8sManager.GetClient(),
+		Scheme: k8sManager.GetScheme(),
+	}).SetupWithManager(k8sManager)
+	Expect(err).ToNot(HaveOccurred())
+
+	go func() {
+		defer GinkgoRecover()
+		err = k8sManager.Start(ctx)
+		Expect(err).ToNot(HaveOccurred(), "failed to run manager")
+	}()
 })
 
 var _ = AfterSuite(func() {
+	cancel()
 	By("tearing down the test environment")
 	err := testEnv.Stop()
 	Expect(err).NotTo(HaveOccurred())
