a2f484009f9a6b6565237fa595edbc03f0f17cf3
Michael Vittrup Larsen <mivl@tv2.dk>
2023-01-18 12:08:32 +0100

    Add additional e2e test

--- test/e2e/controller_self_test.go
+++ test/e2e/controller_self_test.go
@@ -24,6 +24,7 @@ package e2esuite
 
 import (
 	"context"
+	"regexp"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -56,7 +57,37 @@ metadata:
 data:
   tier2GatewayClass: istio`
 
-var _ = Describe("GatewayClass controller", func() {
+// example.com does resolve to an IP address so it is not ideal for testing
+const gatewayManifest string = `
+apiVersion: gateway.networking.k8s.io/v1beta1
+kind: Gateway
+metadata:
+  name: foo-gateway
+  namespace: default
+spec:
+  gatewayClassName: cloud-gw
+  listeners:
+  - name: prod-web
+    port: 80
+    protocol: HTTP
+    hostname: example-foo4567.com`
+
+const httprouteManifest string = `
+apiVersion: gateway.networking.k8s.io/v1beta1
+kind: HTTPRoute
+metadata:
+  name: foo-site
+  namespace: default
+spec:
+  parentRefs:
+  - kind: Gateway
+    name: foo-gateway
+  rules:
+  - backendRefs:
+    - name: foo-site
+      port: 80`
+
+var _ = Describe("GatewayClass", func() {
 
 	const (
 		timeout  = time.Second * 10
@@ -70,12 +101,12 @@ var _ = Describe("GatewayClass controller", func() {
 
 			gwc := &gatewayapi.GatewayClass{}
 			err := yaml.Unmarshal([]byte(gatewayclassManifest), gwc)
-			Expect(err).Should(Succeed())
-			Expect(k8sClient.Create(ctx, gwc)).Should(Succeed())
+			Expect(err).To(Succeed())
+			Expect(k8sClient.Create(ctx, gwc)).To(Succeed())
 
 			cm := &corev1.ConfigMap{}
 			Expect(yaml.Unmarshal([]byte(gwClassConfigMapManifest), cm)).To(Succeed())
-			Expect(k8sClient.Create(ctx, cm)).Should(Succeed())
+			Expect(k8sClient.Create(ctx, cm)).To(Succeed())
 
 			lookupKey := types.NamespacedName{Name: gwc.ObjectMeta.Name, Namespace: ""}
 			gwcRead := &gatewayapi.GatewayClass{}
@@ -83,6 +114,7 @@ var _ = Describe("GatewayClass controller", func() {
 			Eventually(func() bool {
 				err := k8sClient.Get(ctx, lookupKey, gwcRead)
 				if err != nil ||
+					len(gwcRead.Status.Conditions) == 0 ||
 					gwcRead.Status.Conditions[0].Type != string(gatewayapi.GatewayClassConditionStatusAccepted) ||
 					gwcRead.Status.Conditions[0].Status != "True" {
 					return false
@@ -90,8 +122,92 @@ var _ = Describe("GatewayClass controller", func() {
 				return true
 			}, timeout, interval).Should(BeTrue())
 
-			Expect(k8sClient.Delete(ctx, cm)).Should(Succeed())
-			Expect(k8sClient.Delete(ctx, gwc)).Should(Succeed())
+			Expect(k8sClient.Delete(ctx, cm)).To(Succeed())
+			Expect(k8sClient.Delete(ctx, gwc)).To(Succeed())
+		})
+	})
+})
+
+var _ = Describe("Gateway addresses", func() {
+
+	const (
+		timeout  = time.Second * 10
+		interval = time.Millisecond * 250
+	)
+	var (
+		gwc          *gatewayapi.GatewayClass
+		cm           *corev1.ConfigMap
+		ip4AddressRe *regexp.Regexp
+		hostnameRe   *regexp.Regexp
+	)
+
+	BeforeEach(func() {
+		ip4AddressRe, _ = regexp.Compile(`^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$`)
+		hostnameRe, _ = regexp.Compile(`^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])(\.([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]))*$`)
+		gwc = &gatewayapi.GatewayClass{}
+		err := yaml.Unmarshal([]byte(gatewayclassManifest), gwc)
+		ctx := context.Background()
+		Expect(err).To(Succeed())
+		Expect(k8sClient.Create(ctx, gwc)).To(Succeed())
+
+		cm = &corev1.ConfigMap{}
+		Expect(yaml.Unmarshal([]byte(gwClassConfigMapManifest), cm)).To(Succeed())
+		Expect(k8sClient.Create(ctx, cm)).To(Succeed())
+	})
+
+	AfterEach(func() {
+		ctx := context.Background()
+		Expect(k8sClient.Delete(ctx, gwc)).To(Succeed())
+		Expect(k8sClient.Delete(ctx, cm)).To(Succeed())
+	})
+
+	Context("When a gateway/httproute is created", func() {
+		It("They be marked as ready/accepted", func() {
+			By("Setting a condition")
+
+			ctx := context.Background()
+			gw := &gatewayapi.Gateway{}
+			Expect(yaml.Unmarshal([]byte(gatewayManifest), gw)).To(Succeed())
+			Expect(k8sClient.Create(ctx, gw)).To(Succeed())
+
+			rt := &gatewayapi.HTTPRoute{}
+			Expect(yaml.Unmarshal([]byte(httprouteManifest), rt)).To(Succeed())
+			Expect(k8sClient.Create(ctx, rt)).To(Succeed())
+
+			lookupKey := types.NamespacedName{Name: gw.ObjectMeta.Name, Namespace: gw.ObjectMeta.Namespace}
+			gwRead := &gatewayapi.Gateway{}
+
+			Eventually(func() bool {
+				err := k8sClient.Get(ctx, lookupKey, gwRead)
+				if err != nil ||
+					len(gwRead.Status.Addresses) != 1 ||
+					(*gwRead.Status.Addresses[0].Type == gatewayapi.IPAddressType && !ip4AddressRe.MatchString(gwRead.Status.Addresses[0].Value)) ||
+					(*gwRead.Status.Addresses[0].Type == gatewayapi.HostnameAddressType && !hostnameRe.MatchString(gwRead.Status.Addresses[0].Value)) {
+					return false
+				}
+				return true
+			}, timeout, interval).Should(BeTrue())
+
+			lookupKey = types.NamespacedName{Name: rt.ObjectMeta.Name, Namespace: rt.ObjectMeta.Namespace}
+			rtRead := &gatewayapi.HTTPRoute{}
+
+			Eventually(func() bool {
+				err := k8sClient.Get(ctx, lookupKey, rtRead)
+				if err != nil ||
+					len(rtRead.Status.RouteStatus.Parents) != 1 ||
+					len(rtRead.Status.RouteStatus.Parents[0].Conditions) != 1 ||
+					string(rtRead.Status.RouteStatus.Parents[0].ParentRef.Name) != gw.ObjectMeta.Name ||
+					string(*rtRead.Status.RouteStatus.Parents[0].ParentRef.Namespace) != gw.ObjectMeta.Namespace ||
+					//rtRead.Status.RouteStatus.Parents[0].ControllerName != "xxx"
+					rtRead.Status.RouteStatus.Parents[0].Conditions[0].Type != string(gatewayapi.RouteConditionAccepted) ||
+					rtRead.Status.RouteStatus.Parents[0].Conditions[0].Status != "True" {
+					return false
+				}
+				return true
+			}, timeout, interval).Should(BeTrue())
+
+			Expect(k8sClient.Delete(ctx, rt)).To(Succeed())
+			Expect(k8sClient.Delete(ctx, gw)).To(Succeed())
 		})
 	})
 })
