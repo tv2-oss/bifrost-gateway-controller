a80ccd4a59c1d5173a54ac7d192b8e517c69ae0a
Michael Vittrup Larsen <mivl@tv2.dk>
2023-03-21 15:28:09 +0100

    Update demo curl script

--- hack/demo/test-curl.sh
+++ hack/demo/test-curl.sh
@@ -3,9 +3,18 @@
 ADDR=`kubectl -n foo-infra get gateway foo-gateway -o jsonpath='{.status.addresses[0].value}'`
 IP=`dig "$ADDR" +short | head -n1`
 
+echo "-------------------------------------------------------------------"
+echo "Skipping DNS, using $ADDR = $IP"
+echo "-------------------------------------------------------------------"
+read -p "Press enter to run CURL commands"
+
+echo "-------------------------------------------------------------------"
+echo ""
 echo "1x curl http://foo.example.com/site"
 curl --resolve foo.example.com:80:$IP http://foo.example.com/site
 
+echo "-------------------------------------------------------------------"
+echo ""
 echo "20x curl http://foo.example.com/store"
 for i in {1..20}
 do
