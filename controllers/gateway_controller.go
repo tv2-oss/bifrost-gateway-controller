9dd8161d90cf04f4d2ba2519d83f9fbfceae9d0d
Michael Vittrup Larsen <mivl@tv2.dk>
2023-03-21 07:59:05 +0100

    Sort status output to avoid reconcile loops

--- controllers/gateway_controller.go
+++ controllers/gateway_controller.go
@@ -34,6 +34,7 @@ package controllers
 import (
 	"context"
 	"fmt"
+	"sort"
 	"strings"
 	"time"
 
@@ -256,6 +257,7 @@ func (r *GatewayReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 		progReason = string(gatewayapi.GatewayReasonProgrammed)
 	} else {
 		missing := statusExistingTemplates(templates)
+		sort.Strings(missing)
 		progMsg = fmt.Sprintf("missing %v resources: %s", len(templates)-existsNum, strings.Join(missing, ","))
 	}
 	meta.SetStatusCondition(&gw.Status.Conditions, metav1.Condition{
