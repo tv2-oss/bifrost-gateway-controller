83c6f624494d92756eb96cb3303fd8745ca16d46
Kent Mikkel Vilhelmsen <60783588+kemv@users.noreply.github.com>
2023-07-26 13:25:40 +0200

    Limited character for name of targetgroup in GatewayClassBlueprint (#221)
    * Limited character binding for targetgroup
    
    * Update README
    
    * add tagging

--- hack/demo/delete-gw-cluster-resources.sh
+++ hack/demo/delete-gw-cluster-resources.sh
@@ -4,6 +4,7 @@ NS=$1
 GWNAME=$2
 
 NAME=gw-${NS}-${GWNAME}
+TARGETGROUPNAME="$(echo "gw-${NS}-${GWNAME}" | cut -b1-25)-$(echo -n "${NS}-${GWNAME}" | openssl sha1 | cut -b1-6)"
 
 kubectl delete securitygrouprule.ec2.aws.upbound.io/${NAME}-upstream15021
 kubectl delete securitygrouprule.ec2.aws.upbound.io/${NAME}-upstream80
@@ -13,6 +14,6 @@ kubectl delete securitygrouprule.ec2.aws.upbound.io/${NAME}-ingress80
 kubectl delete securitygrouprule.ec2.aws.upbound.io/${NAME}-ingress443
 kubectl delete lblistener.elbv2.aws.upbound.io/${NAME}
 kubectl delete lblistener.elbv2.aws.upbound.io/${NAME}-redir
-kubectl delete lbtargetgroup.elbv2.aws.upbound.io/${NAME}
+kubectl delete lbtargetgroup.elbv2.aws.upbound.io/${TARGETGROUPNAME}
 kubectl delete lb.elbv2.aws.upbound.io/${NAME}
 kubectl delete securitygroup.ec2.aws.upbound.io/${NAME}
