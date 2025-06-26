9edcbb02784c00b018e098baa0e2f1402344cb02
Michael Vittrup Larsen <mivl@tv2.dk>
2025-06-26 06:17:42 +0200

    update tooling, go version 1.24 (#310)
    * update tooling, go version 1.24
    
    * update crds in chart

--- scripts/helm-docs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-## Reference: https://github.com/norwoodj/helm-docs
-set -eux
-
-echo "Running Helm-Docs"
-docker run \
-    -v "$(pwd):/helm-docs" \
-    -u $(id -u) \
-    jnorwood/helm-docs:v1.11.0
